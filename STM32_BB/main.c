208f9d66c8a3526fb22885bc252cb579f9d704ac
Michal Demin <michaldemin@gmail.com>
2010-03-01 18:26:38 +0100

    STM32_BB: minor changes in main.c
    Signed-off-by: Michal Demin <michaldemin@gmail.com>

--- STM32_BB/main.c
+++ STM32_BB/main.c
@@ -7,23 +7,20 @@
 
 #include "platform.h"
 
-
-void Delay_ (unsigned long a) { while (--a!=0); }
-
 #ifdef VECT_TAB_RAM
 /* vector-offset (TBLOFF) from bottom of SRAM. defined in linker script */
 extern uint32_t _isr_vectorsram_offs;
 void NVIC_Configuration(void)
 {
-  /* Set the Vector Table base location at 0x20000000+_isr_vectorsram_offs */
-  NVIC_SetVectorTable(NVIC_VectTab_RAM, (uint32_t)&_isr_vectorsram_offs);
+	/* Set the Vector Table base location at 0x20000000+_isr_vectorsram_offs */
+	NVIC_SetVectorTable(NVIC_VectTab_RAM, (uint32_t)&_isr_vectorsram_offs);
 }
 #else
 extern uint32_t _isr_vectorsflash_offs;
 void NVIC_Configuration(void)
 {
-  /* Set the Vector Table base location at 0x08000000+_isr_vectorsflash_offs */
-  NVIC_SetVectorTable(NVIC_VectTab_FLASH, (uint32_t)&_isr_vectorsflash_offs);
+	/* Set the Vector Table base location at 0x08000000+_isr_vectorsflash_offs */
+	NVIC_SetVectorTable(NVIC_VectTab_FLASH, (uint32_t)&_isr_vectorsflash_offs);
 }
 #endif /* VECT_TAB_RAM */
 
@@ -44,24 +41,26 @@ void GPIO_Configuration(void)
 	/* disable JTAG !!! */
 //	GPIO_PinRemapConfig(GPIO_Remap_SWJ_Disable, ENABLE);
 
-	/* set LED, SPI int as Open-drive */
+	/* set LED, SPI int as Open-drain */
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-	/* set LED, SPI int as Open-drive */
+	/* set LED, SPI int as Open-drain */
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_OD;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
+
+	/*TODO:  should this be moved to power.c ? */
 	/* PB6 - ACSEL, PB7 - ENABLE */
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_Init(GPIOB, &GPIO_InitStructure);
-	
+
 	/* PB2 - ACPRES, PB5 - ALARM */
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_5;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
@@ -75,6 +74,8 @@ void GPIO_Configuration(void)
 	LED_RED(Bit_SET);
 	SPI_INT_WRITE(Bit_SET);
 
+	PWR_ENABLE(Bit_RESET);
+	PWR_ACSEL(Bit_RESET);
 
 }
 
@@ -88,11 +89,12 @@ int main(void)
 
 	/* Configure the GPIO ports */
 	GPIO_Configuration();
-	
+
 	SYS_Init();
+	PWR_Init();
 	CANController_Init();
 	SPI_Slave_Init();
-	
+
 	vTaskStartScheduler();
 
 	while (1);
