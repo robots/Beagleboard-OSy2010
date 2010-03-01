72f32cbc74ce4e8edaf8ec299ec65662413e3267
Michal Demin <michaldemin@gmail.com>
2010-03-01 17:04:05 +0100

    STM32_BB: change platform dependent files
    Signed-off-by: Michal Demin <michaldemin@gmail.com>

--- STM32_BB/.gdbinit
+++ STM32_BB/.gdbinit
@@ -4,10 +4,5 @@ file FLASH_RUN/stm32_blink_rtc.elf
 
 target remote localhost:3333
 
-echo
-echo Issueing reseting Cortex m3
-echo
-echo
-
 monitor soft_reset_halt
 
--- STM32_BB/platform.h
+++ STM32_BB/platform.h
@@ -13,6 +13,17 @@
 #define SPI_INT_PIN        GPIO_Pin_8
 #define SPI_INT_GPIO       GPIOA
 
+#define PWR_ENABLE_PIN     GPIO_Pin_7
+#define PWR_ENABLE_GPIO    GPIOB
+
+#define PWR_ACSEL_PIN      GPIO_Pin_6
+#define PWR_ACSEL_GPIO     GPIOB
+
+#define PWR_ALARM_PIN      GPIO_Pin_5
+#define PWR_ALARM_GPIO     GPIOB
+
+#define PWR_ACPRES_PIN     GPIO_Pin_5
+#define PWR_ACPRES_GPIO    GPIOB
 
 #define LED_YELLOW(x)      GPIO_WriteBit(LED_YELLOW_GPIO, LED_YELLOW_PIN, x)
 #define LED_GREEN(x)       GPIO_WriteBit(LED_GREEN_GPIO, LED_GREEN_PIN, x)
@@ -20,6 +31,10 @@
 
 #define SPI_INT_WRITE(x)   GPIO_WriteBit(SPI_INT_GPIO, SPI_INT_PIN, x)
 
-#define TAMP_READ          GPIO_ReadInputDataBit(TAMP_GPIO, TAMP_PIN)
+#define PWR_ENABLE(x)      GPIO_WriteBit(PWR_ENABLE_GPIO, PWR_ENABLE_PIN, x)
+#define PWR_ACSEL(x)       GPIO_WriteBit(PWR_ACSEL_GPIO, PWR_ACSEL_PIN, x)
+
+#define PWR_ALARM()        GPIO_ReadInputDataBit(PWR_ALARM_GPIO, PWR_ALARM_PIN)
+#define PWR_ACPRES()       GPIO_ReadInputDataBit(PWR_ACPRES_GPIO, PWR_ACPRES_PIN)
 #endif
 
