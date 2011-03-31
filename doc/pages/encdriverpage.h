08e90ac7bd2cb7f12c49bc05fc5df266e951df22
Kuba Marek <blue.cube@seznam.cz>
2011-03-31 22:21:39 +0200

    doc: Added documentation pages to doxygen doc.

---
+++ doc/pages/encdriverpage.h
@@ -0,0 +1,8 @@
+/**
+\file This file contains only documentation page for doxygen 
+
+\page encdriverpage Linux driver for Microchip ENC424j600 SPI Ethernet controller
+
+Some fun talk about beagleboard and STM32 board.
+
+*/
---
+++ doc/pages/mainpage.h
@@ -0,0 +1,13 @@
+/**
+\file This file contains only documentation page for doxygen 
+
+\mainpage
+
+Our project for OSy was writing software for BeagleBoard with some additional custom
+hardware. Description of our HW can be found on \ref hwpage "this page"
+
+Software part of our project consists of:
+- \ref stmpage "firmware for the stm32 board"
+- \ref stmdriverpage "linux driver for the board"
+- \ref encdriverpage "linux driver for enc424j600 network chip"
+*/
