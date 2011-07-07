c8e0aab6942cb7f17fdf1ac56d5899b2e9acb2e5
Kuba Marek <blue.cube@seznam.cz>
2011-07-07 10:11:08 +0200

    doc: Some writing in hw page.

--- doc/pages/hwpage.h
+++ doc/pages/hwpage.h
@@ -3,11 +3,16 @@
 
 \page hwpage Hardware description
 
+\image html main_small.jpg "From top to bottom: USB hub, expansion board and Beagleboard."
+
+
 \section hwpagebeagle Beagleboard
 The system is based on Beagleboard. Low cost OMAP3530 development board. More about this board can be found here http://beagleboard.org
 Beagleboard provides expansion connector with multiple processor peripherals exposed - SPI, MMC, GPIO, etc.
 
-Our custom expansion board contains ethernet controller chip (with embedded MAC + PHY), STM32 mircocontroller,
+\section hwpageexpansion Expansion Board
+
+Our custom expansion board contains \ref hwpageenc "ethernet controller chip", STM32 mircocontroller,
 battery managenment. This board provides CAN bus and Ethernet connectivity, battery charging and
 power managenment for the whole system.
 
@@ -17,7 +22,19 @@ power managenment for the whole system.
 
 - popisat ako funguje nabijacka
 
-\section hwpageenc Microchip ENC424j600
+Our expansion board connects to the epansion header of Beagleboard, adds
+several peripherials and exposes more UART and I2C interfaces of Beagleboard.
+
+- Real time clock with battery
+- \ref hwpageenc "10/100Mbit Ethernet"
+- STM32 MCU (Cortex-M3 based)
+	- CAN bus
+	- Battery/power management
+Exposes more BB peripherals - UART, I2C
+
+\image html system.svg "Expansion Board Connections"
+
+\subsection hwpageenc Microchip ENC424j600
 Microchip ENC424j600 is an fast ethernet controller with integrated MAC and PHY.
 It is designed to be easily added to a microcontroller only with a minimum external
 components.
@@ -28,8 +45,5 @@ Its features include:
 - SPI and parallel interface
 
 The chip is connected via the SPI interface and uses one interrupt channel.
-
-
-\image html system.svg "Expansion Board Connections"
 
 */
